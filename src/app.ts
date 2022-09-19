24ef5c1976ff59cd2a78e69c7e1012132ea75bc8
brightdevs <38365682+e-rojas@users.noreply.github.com>
2022-09-19 14:54:22 -0300

    card styling created

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,16 @@ class Card {
   image: string;
 
   constructor(
-    public brandType: string,
-    public title: string,
-    public description: string
+    private brandType: string,
+    private title: string,
+    private description: string,
+    private price: number,
+    private discount: number
   ) {
     this.image = './images/image-product-mobile.jpg';
+
+    // what percentage of discount is 20 dollars off 169.99
+    // 20 / 169.99 = 0.117
 
     this.hostElement = document.getElementById('app') as HTMLElement;
     this.cardElement = document.createElement('main') as HTMLElement;
@@ -26,6 +31,17 @@ class Card {
                 <span class="card__brand">${this.brandType}</span>
                 <h2 class="card__title">${this.title}</h2>
                 <p class="card__description">${this.description}</p>
+                <div class="card__price">
+                    <span class="card__price--discounted">$${
+                      this.price - this.discount
+                    }</span>
+                    <span class="card__price--original">$${this.price}</span>
+                </div>
+                <button class="card__button">
+                <img src='./images/icon-cart.svg' class="card__button--icon"></img>
+                Add to Cart
+                </button>
+
               </div>
          `;
     this.hostElement.appendChild(this.cardElement);
@@ -48,5 +64,7 @@ class Card {
 const card = new Card(
   'Perfume',
   'Gabrielle Essence Eau De Parfum',
-  'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.'
+  'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+  169.99,
+  20
 );
